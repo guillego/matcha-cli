1f86d99ba4a700e18bcb82ec7d292678cd9bf242
Guillermo Galan <ggo@guillego.com>
2024-07-13 13:51:29 +0200

    implement a simple test

---
+++ test/index.test.ts
@@ -0,0 +1,9 @@
+import { execSync } from 'child_process';
+import { describe, it, expect } from 'vitest';
+
+describe('CLI', () => {
+  it('should greet the user', () => {
+    const output = execSync('node dist/src/index.js greet Tester').toString();
+    expect(output.trim()).toBe('Howdy, Tester!');
+  });
+});
