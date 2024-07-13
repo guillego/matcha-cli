b2b716df46db047a04d41040da465b23ab252f55
Guillermo Galan <ggo@guillego.com>
2024-07-13 17:28:04 +0200

    add more options to cli

--- src/cli.ts
+++ src/cli.ts
@@ -21,12 +21,31 @@ function createCLI(version: string): Command {
  */
 export function setupCLI(version: string) {
   const cli = createCLI(version);
+  
+  cli
+    .command("greet [name]")
+    .description("Greet someone by name")
+    .option("--cowboy", "Greet with a cowboy style")
+    .option("--australian", "Greet with an Australian style")
+    .action((name: string | undefined, options: any) => {
+      const greeting = name ? name : "pal";
+
+      if (options.cowboy && options.australian) {
+        console.log(`Please choose only one style: --cowboy or --australian.`);
+      } else if (options.cowboy) {
+        console.log(`Howdy, ${greeting}!`);
+      } else if (options.australian) {
+        console.log(`G'day, ${greeting}!`);
+      } else {
+        console.log(`Hello, ${greeting}!`);
+      }
+    });
 
   cli
-    .command("greet <name>")
-    .description("Greet someone by name")
-    .action((name: string) => {
-      console.log(`Howdy, ${name}!`);
+    .command("bye")
+    .description("Say goodbye")
+    .action(() => {
+      console.log("Farewell, friend!");
     });
 
   return cli;
